efe4386b0a2e0b8ad6acc8e303fd4f3f10af3aa8
Emil Latypov <emillatypov9335@gmail.com>
2023-03-11 22:39:16 +0300

    hw2: Remove debug info from early development stages

--- lab2/runner.c
+++ lab2/runner.c
@@ -9,12 +9,6 @@
 #include "parser.h"
 #include "runner.h"
 
-static const char *JOB_OPERATOR_STR[] = {
-    [OP_AND] = "AND",
-    [OP_OR] = "OR",
-    [OP_BG] = "BG",
-};
-
 int chain_jobs(struct shell_job *_jobs, int _num_jobs) {
 
     int num_jobs = 0;
@@ -69,15 +63,6 @@ int chain_jobs(struct shell_job *_jobs, int _num_jobs) {
 
 int run_job(struct shell_job *job) {
     
-    /* DEBUG INFO START */
-    /*
-    printf("(num_tokens=%d, operator=%s)", job->num_tokens, JOB_OPERATOR_STR[job->operator]);
-    for (int j = 0; j < job->num_tokens; ++j) {
-        printf(" <%s>", job->tokens[j]);
-    }
-    printf("\n");
-    */
-    /* DEBIG INFO STOP */
     int ret_value = 0;
     int (*fds)[2] = NULL;
     int *pids;
@@ -89,20 +74,6 @@ int run_job(struct shell_job *job) {
         ret_value = 1;
         goto end;
     }
-
-    /* DEBUG INFO START */
-    /*
-    printf("num_cmds=%d\n", num_cmds);
-    for (int i = 0; i < num_cmds; ++i) {
-        printf("(%d, argc=%d, name=%s, output_file=%s, is_append=%d) ", 
-                i, cmds[i].argc, cmds[i].name, cmds[i].output_fname, cmds[i].output_append);
-
-        for (int j = 0; j < cmds[i].argc; ++j)
-            printf(" {%s}", cmds[i].args[j]);
-        printf("\n");
-    }
-    */
-    /* DEBIG INFO STOP */
 
     pids = malloc(sizeof(int) * num_cmds);
     fds = malloc(sizeof(int[2]) * num_cmds);
